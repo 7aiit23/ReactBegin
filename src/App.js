0bb30f48e5cb6f67726ede428c3dd14896262a1e
7aiit23 <taintde170233@fpt.edu.vn>
2023-06-04 10:12:21 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import { Component } from 'react';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+
   render() {
+
     return (
       <div className="App">
         <Navbar dark color="primary">
@@ -13,7 +23,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
